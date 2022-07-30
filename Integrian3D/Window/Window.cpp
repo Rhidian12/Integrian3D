f8833fe43581021804edbb73705d348a61fac282
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-07-30 21:02:26 +0200

    implement constructor

---
+++ Integrian3D/Window/Window.cpp
@@ -0,0 +1,38 @@
+#include "Window.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+namespace Integrian3D
+{
+	Window::Window(const int width, const int height)
+	{
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+		GLFWwindow* pWindow = glfwCreateWindow(width, height, "Integrian3D", nullptr, nullptr);
+		
+		if (!pWindow)
+		{
+			/* [TODO]: Replace this with logger */
+			std::cout << "Failed to create GLFW window\n";
+
+			glfwTerminate();
+			return;
+		}
+
+		glfwMakeContextCurrent(pWindow);
+
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+		{
+			std::cout << "Failed to initialize GLAD\n";
+			return;
+		}
+
+		glViewport(0, 0, width, height);
+	}
+}
---
+++ Integrian3D/Window/Window.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Integrian3D
+{
+	class Window final
+	{
+	public:
+		explicit Window(const int width, const int height);
+	};
+}
