470b590d8781b19a0ce585900555fb7ded1b92ff
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-01 08:07:21 +0200

    implement GetInstance()

--- Integrian3D/InputManager/InputManager.cpp
+++ Integrian3D/InputManager/InputManager.cpp
@@ -2,5 +2,20 @@
 
 namespace Integrian3D
 {
+	InputManager::InputManager()
+	{}
 
+	InputManager& InputManager::GetInstance()
+	{
+		if (!Instance)
+		{
+			Instance = std::make_unique<InputManager>();
+		}
+
+		return *Instance.get();
+	}
+
+	void InputManager::ProcessInput()
+	{
+	}
 }
--- Integrian3D/InputManager/InputManager.h
+++ Integrian3D/InputManager/InputManager.h
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <memory> /* std::unique_ptr */
+
 namespace Integrian3D
 {
-	class InputManager
+	class InputManager final
 	{
+	public:
+		static InputManager& GetInstance();
+
+		void ProcessInput();
+
+	private:
+		InputManager();
+
+		friend std::unique_ptr<InputManager> std::make_unique<InputManager>();
+		inline static std::unique_ptr<InputManager> Instance{};
 	};
 }
