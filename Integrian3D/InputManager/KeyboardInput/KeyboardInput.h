0ab4ab9c407095ee3dfe5035c0ce6a51843501b5
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-01 08:36:28 +0200

    implement KeyboardInput

---
+++ Integrian3D/InputManager/KeyboardInput/KeyboardInput.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#ifdef _WIN32
+#include <WinUser.h> /* Virtual Keys */
+#endif
+
+namespace Integrian3D
+{
+#ifdef _WIN32
+	enum class KeyboardInput
+	{
+		A = 'A',
+		B = 'B',
+		C = 'C',
+		D = 'D',
+		E = 'E',
+		F = 'F',
+		G = 'G',
+		H = 'H',
+		I = 'I',
+		J = 'J',
+		K = 'K',
+		L = 'L',
+		M = 'M',
+		N = 'N',
+		O = 'O',
+		P = 'P',
+		Q = 'Q',
+		R = 'R',
+		S = 'S',
+		T = 'T',
+		U = 'U',
+		V = 'V',
+		W = 'W',
+		X = 'X',
+		Y = 'Y',
+		Z = 'Z',
+		Left = VK_LEFT,
+		Up = VK_UP,
+		Right = VK_RIGHT,
+		Down = VK_DOWN,
+		Space = VK_SPACE,
+		LShift = VK_LSHIFT,
+		RShift = VK_RSHIFT,
+		LControl = VK_LCONTROL,
+		RControl = VK_RCONTROL
+	};
+#elif
+#error There is no support for platforms outside Windows!
+#endif
+}
