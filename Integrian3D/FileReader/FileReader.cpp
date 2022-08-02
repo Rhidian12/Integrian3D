07fecab22351a8525be0c55bef234d1007982d8f
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-02 16:03:49 +0200

    finish basic FileReader

--- Integrian3D/FileReader/FileReader.cpp
+++ Integrian3D/FileReader/FileReader.cpp
@@ -1,6 +1,51 @@
 #include "FileReader.h"
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+
+#include "../DebugUtility/DebugUtility.h"
+
 namespace Integrian3D
 {
+	FileReader::FileReader(const std::string& filePath)
+		: Handle{}
+		, FilePath{ filePath }
+	{
+		/* open the file */
+		Handle = CreateFileA(filePath.c_str(),
+				GENERIC_READ,
+				FILE_SHARE_READ,
+				nullptr,
+				OPEN_EXISTING,
+				FILE_ATTRIBUTE_NORMAL,
+				nullptr);
 
+		if (Handle == INVALID_HANDLE_VALUE)
+		{
+			Debug::LogError("FileReader could not open the provided file", false);
+			return;
+		}
+
+		/* Get the file size */
+		const DWORD fileSize{ GetFileSize(Handle, nullptr) };
+		FileContents.resize(fileSize);
+
+		/* Read the file contents */
+		DWORD readBytes{};
+		if (ReadFile(Handle, FileContents.data(), fileSize, &readBytes, nullptr) == 0)
+		{
+			Debug::LogError("FileReader could not read the provided file", false);
+			return;
+		}
+	}
+
+	FileReader::~FileReader()
+	{
+		if (CloseHandle(Handle) == 0)
+		{
+			Debug::LogError("FileReader could not close the provided file", false);
+		}
+	}
 }
--- Integrian3D/FileReader/FileReader.h
+++ Integrian3D/FileReader/FileReader.h
@@ -1,8 +1,22 @@
 #pragma once
 
+#include <string> /* std::string */
+#include <vector> /* std::vector */ /* [CRINGE]: Use my own implementation */
+
 namespace Integrian3D
 {
 	class FileReader final
 	{
+	public:
+		explicit FileReader(const std::string& filePath);
+		~FileReader();
+
+		const std::string& GetFilePath() const { return FilePath; }
+		const std::string& GetFileContents() const { return FileContents; }
+
+	private:
+		void* Handle; /* HANDLE */
+		std::string FilePath;
+		std::string FileContents;
 	};
 }
