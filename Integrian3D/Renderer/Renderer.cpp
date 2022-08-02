68a51fda93e16d236cfc77e3f69f097b2149ef6e
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-02 15:26:07 +0200

    implement GetInstance()

--- Integrian3D/Renderer/Renderer.cpp
+++ Integrian3D/Renderer/Renderer.cpp
@@ -2,5 +2,16 @@
 
 namespace Integrian3D
 {
+	Renderer::Renderer()
+	{}
 
+	Renderer& Renderer::GetInstance()
+	{
+		if (!Instance)
+		{
+			Instance = std::make_unique<Renderer>();
+		}
+
+		return *Instance.get();
+	}
 }
--- Integrian3D/Renderer/Renderer.h
+++ Integrian3D/Renderer/Renderer.h
@@ -1,8 +1,18 @@
-#pragma once
+#pragma once 
+
+#include <memory> /* std::unique_ptr */
 
 namespace Integrian3D
 {
 	class Renderer final
 	{
+	public:
+		static Renderer& GetInstance();
+
+	private:
+		Renderer();
+
+		friend std::unique_ptr<Renderer> std::make_unique();
+		inline static std::unique_ptr<Renderer> Instance{};
 	};
 }
