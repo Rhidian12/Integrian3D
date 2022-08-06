c8b786503ddf2063041cad5608e111da351b1579
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-06 15:50:40 +0200

    implement Utils

--- Integrian3D/Utils/Utils.cpp
+++ Integrian3D/Utils/Utils.cpp
@@ -4,6 +4,9 @@ namespace Integrian3D
 {
 	namespace Utils
 	{
-
+		constexpr size_t ConstexprStringHash(const char* pString)
+		{
+			return CRC32<sizeof(pString) - 2>(pString) ^ 0xFFFFFFFF;
+		}
 	}
 }
