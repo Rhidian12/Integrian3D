0d00f94501659b36438b04c30564e25ea3363a48
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-06 15:50:58 +0200

    implement ComponentIDGenerator

--- Integrian3D/ComponentIDGenerator/ComponentIDGenerator.h
+++ Integrian3D/ComponentIDGenerator/ComponentIDGenerator.h
@@ -1,6 +1,19 @@
 #pragma once
 
+#include "../EngineConstants/EngineConstants.h"
+#include "../Utils/Utils.h"
+
 namespace Integrian3D
 {
+	template<typename T>
+	constexpr ComponentType GenerateComponentID()
+	{
+		using namespace Utils;
 
+		constexpr const char* typeName(ConstexprTypeName<T>());
+
+		constexpr ComponentType hash(static_cast<ComponentType>(ConstexprStringHash(typeName)));
+
+		return hash;
+	}
 }
