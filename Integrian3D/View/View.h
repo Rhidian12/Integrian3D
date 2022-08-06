8e3c6c806bcd3ba0bb077ff81352929733295283
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-06 15:44:10 +0200

    implement  View

--- Integrian3D/View/View.h
+++ Integrian3D/View/View.h
@@ -1,6 +1,41 @@
 #pragma once
 
+#include <vector> /* std::vector */
+#include <utility> /* std::move(), ... */
+#include <functional> /* std::function */
+
 namespace Integrian3D
 {
+	template<typename ... TComponents>
+	class View final
+	{
+		using ViewContainerType = std::tuple<std::vector<TComponents>&...>;
 
+	public:
+		explicit View(ViewContainerType&& components)
+			: Components{ std::move(components) }
+			, NrOfComponents{ std::get<0>(Components).size() }
+		{}
+
+		void ForEach(const std::function<void(TComponents&...)>& function)
+		{
+			auto indexSequence{ std::make_index_sequence<sizeof ... (TComponents)>{} };
+
+			for (size_t i{}; i < NrOfComponents; ++i)
+			{
+				ForEach(function, i, indexSequence);
+			}
+		}
+
+	private:
+		template<size_t ... Indices>
+		void ForEach(const std::function<void(TComponents&...)>& function, size_t index, std::index_sequence<Indices...>)
+		{
+			auto tuple{ std::tuple<TComponents&...>(std::get<Indices>(Components)[index]...) };
+			std::apply(function, tuple);
+		}
+
+		ViewContainerType Components;
+		size_t NrOfComponents;
+	};
 }
