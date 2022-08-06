f4ed95ed2b60906dea8adb7ff3a12959af05e8d0
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-06 15:40:11 +0200

    add ECS constants

--- Integrian3D/EngineConstants/EngineConstants.h
+++ Integrian3D/EngineConstants/EngineConstants.h
@@ -1,6 +1,19 @@
 #pragma once
 
+#include <cstdint> /* uint64_t */
+#include <limits> /* std::numeric_limits */
+#include <bitset> /* std::bitset */
+
 namespace Integrian3D
 {
+	using Entity = uint32_t; /* This supports ~4 billion possible entities, seems like enough */
+	using ComponentType = uint16_t; /* This supports ~56k possible component types, seems like enough */
 
+	constexpr Entity MaxEntities{ std::numeric_limits<Entity>::max() - 1 };
+	constexpr ComponentType MaxComponentTypes{ std::numeric_limits<ComponentType>::max() - 1 };
+
+	using EntitySignature = std::bitset<MaxComponentTypes>;
+
+	constexpr Entity InvalidEntityID{ std::numeric_limits<Entity>::max() };
+	constexpr ComponentType InvalidComponentID{ std::numeric_limits<ComponentType>::max() };
 }
