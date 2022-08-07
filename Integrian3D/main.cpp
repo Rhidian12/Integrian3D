5bb8a6eaa8b2b72469f19b3184a71fd702f984aa
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-07 14:56:15 +0200

    add test scene code to main

--- Integrian3D/main.cpp
+++ Integrian3D/main.cpp
@@ -1,10 +1,34 @@
 #include "Core/Core.h"
+#include "Scene/Scene.h"
+#include "SceneManager/SceneManager.h"
+#include "MeshComponent/MeshComponent.h"
 
 int main()
 {
 	using namespace Integrian3D;
 
 	Core core{ 640,480 };
+
+	Scene testScene{ "TestScene" };
+
+	std::vector<float> vertices =
+	{
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+
+	std::vector<uint32_t> indices =
+	{
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	Entity entity = testScene.CreateEntity();
+	testScene.AddComponent<MeshComponent>(entity, vertices, indices);
+
+	SceneManager::GetInstance().AddScene(std::move(testScene));
 
 	core.Run();
 }
