049e1599c2ada941d833bec5fa34f3b98196260e
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-07 13:02:42 +0200

    make SceneManager a singleton

--- Integrian3D/SceneManager/SceneManager.cpp
+++ Integrian3D/SceneManager/SceneManager.cpp
@@ -2,5 +2,16 @@
 
 namespace Integrian3D
 {
+	SceneManager::SceneManager()
+	{}
 
+	SceneManager& SceneManager::GetInstance()
+	{
+		if (!Instance)
+		{
+			Instance = std::make_unique<SceneManager>();
+		}
+
+		return *Instance.get();
+	}
 }
--- Integrian3D/SceneManager/SceneManager.h
+++ Integrian3D/SceneManager/SceneManager.h
@@ -1,8 +1,18 @@
 #pragma once
 
+#include <memory> /* std::unique_ptr */
+
 namespace Integrian3D
 {
 	class SceneManager final
 	{
+	public:
+		static SceneManager& GetInstance();
+
+	private:
+		SceneManager();
+
+		friend std::unique_ptr<SceneManager> std::make_unique();
+		inline static std::unique_ptr<SceneManager> Instance{};
 	};
 }
