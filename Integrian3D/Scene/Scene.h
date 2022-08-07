4463d0b5de5546f7bfc31966f0d9320ae8035760
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-07 12:50:56 +0200

    add all ECS functionality to Scene

--- Integrian3D/Scene/Scene.h
+++ Integrian3D/Scene/Scene.h
@@ -15,6 +15,24 @@ namespace Integrian3D
 
 		void AddSceneInitialisation(const std::function<void(const Scene&)>& fn) { InitializeCallback = fn; }
 
+		/* -------------- Begin of ECS Functionality -------------- */
+		Entity CreateEntity() { return Registry.CreateEntity(); }
+		bool ReleaseEntity(const Entity entity) { return Registry.ReleaseEntity(entity); }
+		template<typename T>
+		T& AddComponent(const Entity entity) { return Registry.AddComponent<T>(entity); }
+		template<typename T, typename ... Ts>
+		T& AddComponent(const Entity entity, Ts&& ... args) { return Registry.AddComponent<T, Ts...>(entity, args...); }
+		template<typename T>
+		void RemoveComponent(const Entity entity) { Registry.RemoveComponent<T>(entity); }
+		template<typename T>
+		T& GetComponent(const Entity entity) { return Registry.GetComponent<T>(entity); }
+		template<typename T>
+		const T& GetComponent(const Entity entity) const { return Registry.GetComponent<T>(entity); }
+		size_t GetAmountOfEntities() const { return static_cast<size_t>(Registry.GetAmountOfEntities()); }
+		const EntitySignature GetEntitySignature(const Entity entity) const { return Registry.GetEntitySignature(entity); }
+		/* -------------- End of ECS Functionality -------------- */
+
+
 	private:
 		std::function<void(const Scene&)> InitializeCallback;
 		Registry Registry;
