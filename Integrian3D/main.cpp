838006957eafc448ad0cb3b765dade3c5819ead5
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-08 18:55:52 +0200

    changed Vertex type

--- Integrian3D/main.cpp
+++ Integrian3D/main.cpp
@@ -11,12 +11,11 @@ int main()
 
 	Scene testScene{ "TestScene" };
 
-	std::vector<float> vertices =
+	std::vector<Vertex> vertices =
 	{
-		 0.5f,  0.5f, 0.0f,  // top right
-		 0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
+		 Vertex{ 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f },   // bottom right
+		 Vertex{ -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f },   // bottom left
+		 Vertex{ 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f }    // top
 	};
 
 	std::vector<uint32_t> indices =
