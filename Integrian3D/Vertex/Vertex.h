070fcc28abc81aeb6cf32e74aaba1dd049787d60
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-08 18:45:44 +0200

    change Position type

--- Integrian3D/Vertex/Vertex.h
+++ Integrian3D/Vertex/Vertex.h
@@ -1,21 +1,21 @@
 #pragma once
 
+#include "../EngineConstants.h"
+
 namespace Integrian3D
 {
 	struct Vertex final
 	{
 		/* [TODO]: Change this type to a Vector3 and a RGBColour */
 		explicit Vertex(const float x, const float y, const float z, const float r, const float g, const float b, const float a = 1.f)
-			: X{ x }
-			, Y{ y }
-			, Z{ z }
+			: Position{ x,y,z }
 			, R{ r }
 			, G{ g }
 			, B{ b }
 			, A{ a }
 		{}
 
-		float X, Y, Z;
+		Vector3f Position;
 		float R, G, B, A;
 	};
 }
