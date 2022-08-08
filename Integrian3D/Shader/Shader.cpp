dd21b5d576d9fe2124f23974782b06943318810c
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-08 23:53:38 +0200

    implement Shader ctor

--- Integrian3D/Shader/Shader.cpp
+++ Integrian3D/Shader/Shader.cpp
@@ -1,6 +1,35 @@
 #include "Shader.h"
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "../FileReader/FileReader.h"
+#include "../DebugUtility/DebugUtility.h"
+
 namespace Integrian3D
 {
+	Shader::Shader(const std::string& filePath)
+		: ShaderID{}
+	{
+		const FileReader reader{ filePath };
 
+		/* Generate VertexShader ID */
+		ShaderID = glCreateShader(GL_VERTEX_SHADER);
+
+		/* Attach the VertexShader code to the ID and compile it */
+		const char* pShaderSource{ reader.GetFileContents().c_str() };
+		glShaderSource(ShaderID, 1, &pShaderSource, nullptr);
+		glCompileShader(ShaderID);
+
+		/* Check if the compilation succeeded */
+		int success{};
+		char infoLog[512]{};
+		glGetShaderiv(ShaderID, GL_COMPILE_STATUS, &success);
+
+		if (!success)
+		{
+			glGetShaderInfoLog(ShaderID, 512, nullptr, infoLog);
+			Debug::LogError(std::string("Shader compilation failed: ") + infoLog, false);
+		}
+	}
 }
--- Integrian3D/Shader/Shader.h
+++ Integrian3D/Shader/Shader.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include <string_view>
+
 namespace Integrian3D
 {
 	class Shader final
 	{
+	public:
+		explicit Shader(const std::string& filePath);
+
+
+	private:
+		uint32_t ShaderID;
 	};
 }
