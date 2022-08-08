7231dd3c60722fd3df3d59b0880daef7dcfc6f88
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-08 23:49:10 +0200

    remove explicit ctors, remove old ctor

--- Integrian3D/Vertex/Vertex.h
+++ Integrian3D/Vertex/Vertex.h
@@ -6,22 +6,14 @@ namespace Integrian3D
 {
 	struct Vertex final
 	{
-		/* [TODO]: Change this type to a Vector3 and a RGBColour */
-		explicit Vertex()
-			: Position{}
-			, Colour{}
-		{}
+		Vertex() = default;
 		explicit Vertex(const Vector3f& position)
 			: Position{ position }
 			, Colour{}
 		{}
-		explicit Vertex(const Vector3f& position, const RGBColour& colour)
+		Vertex(const Vector3f& position, const RGBColour& colour)
 			: Position{ position }
 			, Colour{ colour }
-		{}
-		explicit Vertex(const float x, const float y, const float z, const float r, const float g, const float b, const float a = 1.f)
-			: Position{ x,y,z }
-			, Colour{ r,g,b,a }
 		{}
 
 		Vector3f Position;
