f322743ba70b34b9d53e325695557cc190b88315
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-08 18:06:26 +0200

    implement Vertex struct

--- Integrian3D/Vertex/Vertex.h
+++ Integrian3D/Vertex/Vertex.h
@@ -2,5 +2,20 @@
 
 namespace Integrian3D
 {
+	struct Vertex final
+	{
+		/* [TODO]: Change this type to a Vector3 and a RGBColour */
+		explicit Vertex(const float x, const float y, const float z, const float r, const float g, const float b, const float a = 1.f)
+			: X{ x }
+			, Y{ y }
+			, Z{ z }
+			, R{ r }
+			, G{ g }
+			, B{ b }
+			, A{ a }
+		{}
 
+		float X, Y, Z;
+		float R, G, B, A;
+	};
 }
