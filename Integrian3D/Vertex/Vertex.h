1bb873976d6d7e64fa09fce4f11bd947867204b3
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-08 18:54:18 +0200

    implement multiple ctors

--- Integrian3D/Vertex/Vertex.h
+++ Integrian3D/Vertex/Vertex.h
@@ -7,6 +7,18 @@ namespace Integrian3D
 	struct Vertex final
 	{
 		/* [TODO]: Change this type to a Vector3 and a RGBColour */
+		explicit Vertex()
+			: Position{}
+			, Colour{}
+		{}
+		explicit Vertex(const Vector3f& position)
+			: Position{ position }
+			, Colour{}
+		{}
+		explicit Vertex(const Vector3f& position, const RGBColour& colour)
+			: Position{ position }
+			, Colour{ colour }
+		{}
 		explicit Vertex(const float x, const float y, const float z, const float r, const float g, const float b, const float a = 1.f)
 			: Position{ x,y,z }
 			, Colour{ r,g,b,a }
