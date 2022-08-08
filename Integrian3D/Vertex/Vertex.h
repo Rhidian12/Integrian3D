92e6dcc222bb701264adb4c1ab81fe92f27aa9d0
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-08 18:52:48 +0200

    change type to RGBColour

--- Integrian3D/Vertex/Vertex.h
+++ Integrian3D/Vertex/Vertex.h
@@ -9,13 +9,10 @@ namespace Integrian3D
 		/* [TODO]: Change this type to a Vector3 and a RGBColour */
 		explicit Vertex(const float x, const float y, const float z, const float r, const float g, const float b, const float a = 1.f)
 			: Position{ x,y,z }
-			, R{ r }
-			, G{ g }
-			, B{ b }
-			, A{ a }
+			, Colour{ r,g,b,a }
 		{}
 
 		Vector3f Position;
-		float R, G, B, A;
+		RGBColour Colour;
 	};
 }
