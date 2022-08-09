a350d4d330190b34f135be91bfce0b4594f273fc
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-09 15:50:33 +0200

    implement Vector2 ctors

--- Integrian3D/Math/Vector2.h
+++ Integrian3D/Math/Vector2.h
@@ -2,5 +2,15 @@
 
 namespace Integrian3D
 {
+	template<typename T>
+	struct Vector2 final
+	{
+		Vector2() = default;
+		Vector2(const T x, const T y)
+			: X{ x }
+			, Y{ y }
+		{}
 
+		T X, Y;
+	};
 }
