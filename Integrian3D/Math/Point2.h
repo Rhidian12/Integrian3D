d2bfafbad4c0b86a17ce259ca09a9dd48b56201d
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-09 15:44:37 +0200

    implement Point2 ctors

--- Integrian3D/Math/Point2.h
+++ Integrian3D/Math/Point2.h
@@ -2,5 +2,15 @@
 
 namespace Integrian3D
 {
+	template<typename T>
+	struct Point2 final
+	{
+		Point2() = default;
+		Point2f(const T x, const T y)
+			: X{ x }
+			, Y{ y }
+		{}
 
+		T X, Y;
+	};
 }
