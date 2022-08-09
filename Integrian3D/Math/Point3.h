3278fd9fbdda8a2b119f4edd8d4add9a84fdcf79
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-09 15:49:01 +0200

    implement Point3 ctors

--- Integrian3D/Math/Point3.h
+++ Integrian3D/Math/Point3.h
@@ -2,5 +2,16 @@
 
 namespace Integrian3D
 {
+	template<typename T>
+	struct Point3 final
+	{
+		Point3() = default;
+		Point3(const T x, const T y, const T z)
+			: X{ x }
+			, Y{ y }
+			, Z{ z }
+		{}
 
+		T X, Y, Z;
+	};
 }
