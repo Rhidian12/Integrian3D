8e367be3c838d0980a9470b19a5a0c1460fdb421
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-10 22:53:29 +0200

    implement Point4

--- Integrian3D/Math/Point4.h
+++ Integrian3D/Math/Point4.h
@@ -2,5 +2,17 @@
 
 namespace Integrian3D
 {
+	template<typename T>
+	struct Point4 final
+	{
+		Point4() = default;
+		Point4(const T x, const T y, const T z, const T w)
+			: X{ x }
+			, Y{ y }
+			, Z{ z }
+			, W{ w }
+		{}
 
+		T X, Y, Z, W;
+	};
 }
