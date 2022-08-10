f6a7a305e616945123d7b571f2b16ef886d2dea0
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-10 11:04:34 +0200

    add ctor

--- Integrian3D/Texture/Texture.cpp
+++ Integrian3D/Texture/Texture.cpp
@@ -2,5 +2,9 @@
 
 namespace Integrian3D
 {
-
+	Texture::Texture(const std::string_view filePath)
+		: Width{}
+		, Height{}
+	{
+	}
 }
--- Integrian3D/Texture/Texture.h
+++ Integrian3D/Texture/Texture.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include <string_view> /* std::string_view */
+
 namespace Integrian3D
 {
 	class Texture final
 	{
+	public:
+		explicit Texture(const std::string_view filePath);
+
+	private:
+		int Width;
+		int Height;
 	};
 }
