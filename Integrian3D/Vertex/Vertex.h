9a6bf0a7f0f2fdfda0620abc3376b7867d4da7f8
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-10 15:15:54 +0200

    add __VERTEX_COLOUR__ to vertex

--- Integrian3D/Vertex/Vertex.h
+++ Integrian3D/Vertex/Vertex.h
@@ -4,19 +4,45 @@
 
 namespace Integrian3D
 {
+#ifdef __VERTEX_COLOUR__
 	struct Vertex final
 	{
 		Vertex() = default;
 		explicit Vertex(const Vector3f& position)
 			: Position{ position }
 			, Colour{}
+			, UV{}
 		{}
 		Vertex(const Vector3f& position, const RGBColour& colour)
 			: Position{ position }
 			, Colour{ colour }
+			, UV{}
+		{}
+		Vertex(const Vector3f& position, const RGBColour& colour, const Point2f& uv)
+			: Position{ position }
+			, Colour{ colour }
+			, UV{ uv }
 		{}
 
 		Vector3f Position;
 		RGBColour Colour;
+		Point2f UV;
 	};
+#else
+	struct Vertex final
+	{
+		Vertex() = default;
+		explicit Vertex(const Vector3f& position)
+			: Position{ position }
+			, UV{}
+		{}
+		Vertex(const Vector3f& position, const Point2f& uv)
+			: Position{ position }
+			, UV{ uv }
+		{}
+
+		Vector3f Position;
+		Point2f UV;
+	};
+#endif
 }
