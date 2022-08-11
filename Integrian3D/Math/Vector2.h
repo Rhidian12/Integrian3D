29d032fa2593b936572043379bcd050a5d0ccff7
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-11 11:11:58 +0200

    wip

--- Integrian3D/Math/Vector2.h
+++ Integrian3D/Math/Vector2.h
@@ -10,18 +10,18 @@ namespace Integrian3D
 	template<typename T>
 	struct Vector2 final
 	{
-		Vector2() = default;
-		Vector2(const T x, const T y)
+		constexpr Vector2() = default;
+		constexpr Vector2(const T x, const T y)
 			: X{ x }
 			, Y{ y }
 		{}
 
-		T Dot(const Vector2& vec) const
+		constexpr T Dot(const Vector2& vec) const
 		{
 			return X * vec.X + Y * vec.Y;
 		}
 
-		T Cross(const Vector2& vec) const
+		constexpr T Cross(const Vector2& vec) const
 		{
 			return X * vec.Y - Y * vec.X;
 		}
