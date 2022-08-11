d5293d33a078752cc9e98ef2b2d8c3c5bdc845ab
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-11 10:52:18 +0200

    mark Point3 as constexpr

--- Integrian3D/Math/Point3.h
+++ Integrian3D/Math/Point3.h
@@ -5,8 +5,8 @@ namespace Integrian3D
 	template<typename T>
 	struct Point3 final
 	{
-		Point3() = default;
-		Point3(const T x, const T y, const T z)
+		constexpr Point3() = default;
+		constexpr Point3(const T x, const T y, const T z)
 			: X{ x }
 			, Y{ y }
 			, Z{ z }
