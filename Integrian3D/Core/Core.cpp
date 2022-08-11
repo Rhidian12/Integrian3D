a1de545bd43263ff178605796bc5cc0cae37329b
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-11 15:28:04 +0200

    update transformcomponents

--- Integrian3D/Core/Core.cpp
+++ Integrian3D/Core/Core.cpp
@@ -6,6 +6,7 @@
 #include "../Renderer/Renderer.h"
 #include "../SceneManager/SceneManager.h"
 #include "../Components/MeshComponent/MeshComponent.h"
+#include "../Components/TransformComponent/TransformComponent.h"
 
 namespace Integrian3D
 {
@@ -28,16 +29,46 @@ namespace Integrian3D
 
 			inputManager.ProcessInput();
 
-			/* [TODO]: Add Scene Update! */
 			Scene& activeScene{ sceneManager.GetActiveScene() };
 
-			renderer.StartRenderLoop();
-
-			View<MeshComponent> renderView{ activeScene.CreateView<MeshComponent>() };
-			renderView.ForEach([&renderer](const MeshComponent& meshComponent)->void
+			/* Entity update */
+			{
 				{
-					renderer.Render(meshComponent);
-				});
+					/* [TODO]: add user defined updates here */
+				}
+
+				/* Transform update */
+				{
+					View<TransformComponent> transformView{ activeScene.CreateView<TransformComponent>() };
+					transformView.ForEach([](TransformComponent& transformComponent)->void
+						{
+							if (transformComponent.bShouldRecalculateTransform)
+							{
+								const Matrix4f translationMatrix{};
+
+								const Point3f& localAngle{ transformComponent.GetLocalAngle() };
+								const Matrix4f rotationMatrix{ Matrix4f::MakeRotationMatrix(localAngle.X, localAngle.Y, localAngle.Z) };
+
+								const Point3f& localScale{ transformComponent.GetLocalScale() };
+								Matrix4f scaleMatrix{ Matrix4f::MakeScaleMatrix(localScale.X, localScale.Y, localScale.Z) };
+
+								transformComponent.SetTransform(translationMatrix * rotationMatrix * scaleMatrix);
+
+								transformComponent.bShouldRecalculateTransform = false;
+							}
+						});
+				}
+			}
+
+			/* Rendering */
+			{
+				renderer.StartRenderLoop();
+				View<MeshComponent> renderView{ activeScene.CreateView<MeshComponent>() };
+				renderView.ForEach([&renderer](const MeshComponent& meshComponent)->void
+					{
+						renderer.Render(meshComponent);
+					});
+			}
 
 			Window.Update();
 		}
