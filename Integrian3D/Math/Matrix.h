acdfe7c65acd8a7d35972a74243b6548cdb7fca4
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-11 09:38:02 +0200

    add operator()

--- Integrian3D/Math/Matrix.h
+++ Integrian3D/Math/Matrix.h
@@ -1,10 +1,23 @@
 #pragma once
 
+#include "Point4.h"
+
 namespace Integrian3D
 {
 	template<int R, int C, typename T>
 	struct Matrix final
 	{
+		Matrix() = default;
+
+		T operator()(const int r, const int c) const
+		{
+			return Data[r][c];
+		}
+		T& operator()(const int r, const int c)
+		{
+			return Data[r][c];
+		}
+
 		T Data[R][C];
 	};
 }
