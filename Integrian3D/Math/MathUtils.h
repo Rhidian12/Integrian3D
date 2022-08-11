7b44ac26d63561c1c4efddedcc5c0dea52fe19be
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-11 14:00:10 +0200

    update Sqrtf

--- Integrian3D/Math/MathUtils.h
+++ Integrian3D/Math/MathUtils.h
@@ -5,7 +5,8 @@ namespace Integrian3D
 	namespace MathUtils
 	{
 		/* magic lol https://www.codeproject.com/Articles/69941/Best-Square-Root-Method-Algorithm-Function-Precisi */
-		inline constexpr float Sqrtf(const float n)
+		/* works only for floats since it depends on IEEE 754 single precision floating point format */
+		__forceinline constexpr float Sqrtf(const float n)
 		{
 			union
 			{
@@ -14,20 +15,7 @@ namespace Integrian3D
 			} u;
 
 			u.x = n;
-			u.i = (1 << 29) + (u.i >> 1) - (1 << 22);
-			return u.x;
-		}
-
-		inline constexpr double Sqrtf(const double n)
-		{
-			union
-			{
-				int i;
-				double x;
-			} u;
-
-			u.x = n;
-			u.i = (1 << 29) + (u.i >> 1) - (1 << 22);
+			u.i = (1 << 29) + (u.i >> 1) - (1 << 22) - 0x4B0D2;
 			return u.x;
 		}
 	}
