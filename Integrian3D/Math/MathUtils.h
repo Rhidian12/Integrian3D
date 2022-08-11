486b9a0b4b874fdec5a6e9f44674a22c29232584
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-11 12:09:36 +0200

    add Sqrtf double overload

--- Integrian3D/Math/MathUtils.h
+++ Integrian3D/Math/MathUtils.h
@@ -11,5 +11,12 @@ namespace Integrian3D
 			_asm fsqrt
 			_asm ret 8
 		}
+
+		double inline __declspec (naked) __fastcall Sqrtf(const double n)
+		{
+			_asm fld qword ptr[esp + 4]
+				_asm fsqrt
+			_asm ret 8
+		}
 	}
 }
