6f4e881b9579126253287a8d8303e4fa287847e8
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-12 13:05:18 +0200

    implenment ToRadians and pi declarations

--- Integrian3D/Math/MathUtils.h
+++ Integrian3D/Math/MathUtils.h
@@ -7,6 +7,10 @@ namespace Integrian3D
 {
 	namespace MathUtils
 	{
+		constexpr double Pi{ 3.14159265358979323846264338327950288 };
+		constexpr double Pi_2{ Pi / 2.0 };
+		constexpr double Pi_4{ Pi / 4.0 };
+
 		/* magic lol https://www.codeproject.com/Articles/69941/Best-Square-Root-Method-Algorithm-Function-Precisi */
 		/* works only for floats since it depends on IEEE 754 single precision floating point format */
 		__forceinline constexpr float Sqrtf(const float n)
@@ -22,6 +26,11 @@ namespace Integrian3D
 			return u.x;
 		}
 
+		template<typename T, typename = std::enable_if_t<std::is_floating_point_v<T>>>
+		constexpr T ToRadians(const T deg)
+		{
+			return deg * Pi / static_cast<T>(180.f);
+		}
 
 		template<typename T, typename = std::enable_if_t<std::is_fundamental_v<T>>>
 		constexpr bool AreEqual(const T a, const T b)
