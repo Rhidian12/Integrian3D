70b9fbefc99e3530e5a3c9ad9ffa67c988451424
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-12 11:01:00 +0200

    delete math typedefs

--- Integrian3D/EngineConstants.h
+++ Integrian3D/EngineConstants.h
@@ -1,11 +1,6 @@
 #pragma once
 
-#include "Math/Point2.h"
-#include "Math/Point3.h"
-#include "Math/Point4.h"
-#include "Math/Vector3.h"
 #include "Math/RGBColour.h"
-#include "Math/Matrix.h"
 
 #include <cstdint> /* uint64_t */
 #include <limits> /* std::numeric_limits */
@@ -25,22 +20,4 @@ namespace Integrian3D
 
 	constexpr Entity InvalidEntityID{ std::numeric_limits<Entity>::max() };
 	constexpr ComponentType InvalidComponentID{ std::numeric_limits<ComponentType>::max() };
-
-	using Point2f = Point2<float>;
-	using Point2d = Point2<double>;
-
-	using Point3f = Point3<float>;
-	using Point3d = Point3<double>;
-
-	using Point4f = Point4<float>;
-	using Point4d = Point4<double>;
-
-	using Vector3f = Vector3<float>;
-	using Vector3d = Vector3<double>;
-
-	using Matrix3f = Matrix<3, 3, float>;
-	using Matrix3d = Matrix<3, 3, double>;
-
-	using Matrix4f = Matrix<4, 4, float>;
-	using Matrix4d = Matrix<4, 4, double>;
 }
