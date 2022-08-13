7fd94f2f2ea2ee172209bd24a338c5a680c3b5ae
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-13 10:40:30 +0200

    update filereader

--- Integrian3D/FileReader/FileReader.h
+++ Integrian3D/FileReader/FileReader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../EngineConstants.h"
+
 #include <string> /* std::string */
 #include <vector> /* std::vector */ /* [CRINGE]: Use my own implementation */
 
@@ -11,8 +13,8 @@ namespace Integrian3D
 		explicit FileReader(const std::string& filePath);
 		~FileReader();
 
-		const std::string& GetFilePath() const { return FilePath; }
-		const std::string& GetFileContents() const { return FileContents; }
+		__NODISCARD const std::string& GetFilePath() const { return FilePath; }
+		__NODISCARD const std::string& GetFileContents() const { return FileContents; }
 
 	private:
 		void* Handle; /* HANDLE */
