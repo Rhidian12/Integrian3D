72bfb40ba28a86e2f9ff33ce2b8088831de59518
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-13 12:31:23 +0200

    add camera ctor

--- Integrian3D/Components/CameraComponent/CameraComponent.cpp
+++ Integrian3D/Components/CameraComponent/CameraComponent.cpp
@@ -2,5 +2,10 @@
 
 namespace Integrian3D
 {
-
+	CameraComponent::CameraComponent(const float nearPlane, const float farPlane, const float fov)
+		: NearPlane{ nearPlane }
+		, FarPlane{ farPlane }
+		, FOV{ fov }
+	{
+	}
 }
--- Integrian3D/Components/CameraComponent/CameraComponent.h
+++ Integrian3D/Components/CameraComponent/CameraComponent.h
@@ -4,5 +4,12 @@ namespace Integrian3D
 {
 	class CameraComponent final
 	{
+	public:
+		CameraComponent(const float nearPlane, const float farPlane, const float fov);
+
+	private:
+		float NearPlane;
+		float FarPlane;
+		float FOV;
 	};
 }
