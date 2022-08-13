f3c85ec4c7fd7a1abc642b095ccd9369e098da0d
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-13 16:44:11 +0200

    add init to CameraComponent

--- Integrian3D/Components/CameraComponent/CameraComponent.cpp
+++ Integrian3D/Components/CameraComponent/CameraComponent.cpp
@@ -7,11 +7,18 @@
 
 namespace Integrian3D
 {
-	CameraComponent::CameraComponent(const float nearPlane, const float farPlane, const float fov)
+	CameraComponent::CameraComponent(const float nearPlane, const float farPlane, const float fov, const float aspectRatio)
 		: NearPlane{ nearPlane }
 		, FarPlane{ farPlane }
 		, FOV{ fov }
+		, AspectRatio{ aspectRatio }
 		, View{ 1.f }
 		, Projection{ 1.f }
 	{}
+
+	void CameraComponent::Start()
+	{
+		View = glm::translate(View, glm::vec3{ 0.f, 0.f, -10.f });
+		Projection = glm::perspective(FOV, AspectRatio, NearPlane, FarPlane);
+	}
 }
--- Integrian3D/Components/CameraComponent/CameraComponent.h
+++ Integrian3D/Components/CameraComponent/CameraComponent.h
@@ -7,12 +7,15 @@ namespace Integrian3D
 	class CameraComponent final
 	{
 	public:
-		CameraComponent(const float nearPlane, const float farPlane, const float fov);
+		CameraComponent(const float nearPlane, const float farPlane, const float fov, const float aspectRatio);
+
+		void Start();
 
 	private:
 		float NearPlane;
 		float FarPlane;
 		float FOV;
+		float AspectRatio;
 
 		glm::mat4 View;
 		glm::mat4 Projection;
