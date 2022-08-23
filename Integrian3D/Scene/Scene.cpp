41376ed5644c6542ac73345d604b785a9cce2975
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-08-23 15:59:20 +0200

    wip

--- Integrian3D/Scene/Scene.cpp
+++ Integrian3D/Scene/Scene.cpp
@@ -27,6 +27,11 @@ namespace Integrian3D
 				100.f,
 				MathUtils::ToRadians(45.f),
 				static_cast<float>(Core::GetInstance().GetWindowWidth()) / Core::GetInstance().GetWindowHeight());
+
+			CreateView<CameraComponent, TransformComponent>().ForEach([](CameraComponent& camera, const TransformComponent& transform)->void
+				{
+
+				});
 		}
 
 		if (InitializeCallback)
