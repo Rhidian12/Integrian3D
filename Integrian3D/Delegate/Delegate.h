479f841a50afe67dcdfdcb1a6623482260be6311
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-10-23 14:13:06 +0200

    Delegate implemented

---
+++ Integrian3D/Delegate/Delegate.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../EngineConstants.h"
+
+#include <functional> /* std::function */
+#include <vector> /* std::vector */
+
+namespace Integrian3D
+{
+	template<typename ... Ts>
+	class Delegate final
+	{
+	public:
+		using Callback = std::function<void(Ts...)>;
+
+		void Bind(const Callback& fn)
+		{
+			Callbacks.push_back(fn);
+		}
+		void Bind(Callback&& fn)
+		{
+			Callbacks.push_back(std::move(fn));
+		}
+
+		template<typename ... Us>
+		void Invoke(Us&&... vals)
+		{
+			for (const Callback& fn : Callbacks)
+			{
+				fn(std::forward<Us>(vals)...);
+			}
+		}
+
+	private:
+		std::vector<Callback> Callbacks;
+	};
+
+}
