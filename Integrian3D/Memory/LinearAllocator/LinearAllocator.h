d224db8880ac65ee3b9a12a14e69a7448a21ecdf
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-10-31 11:32:26 +0100

    add base linear allocator

---
+++ Integrian3D/Memory/LinearAllocator/LinearAllocator.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "../../EngineConstants.h"
+
+namespace Integrian3D::Memory
+{
+	class LinearAllocator final
+	{
+	public:
+		explicit LinearAllocator();
+		explicit LinearAllocator(const size_t nrOfBytes);
+		~LinearAllocator();
+
+		template<typename T>
+		T* allocate(const size_t nrOfElements)
+		{
+			if (nrOfElements == 0)
+			{
+				throw std::invalid_argument{ "LinearAllocator::Allocate() > Cannot allocate 0 elements" };
+			}
+
+			if (reinterpret_cast<T*>(pCurrent) + nrOfElements >= pEnd)
+			{
+				throw std::bad_alloc{};
+			}
+
+			void* pData{ pCurrent };
+
+			pCurrent = reinterpret_cast<T*>(pCurrent) + nrOfElements;
+
+			return reinterpret_cast<T*>(pData);
+		}
+
+		template<typename T>
+		void deallocate([[maybe_unused]] T* p)
+		{
+			throw std::bad_function_call{};
+		}
+
+		size_t capacity() const { return reinterpret_cast<char*>(pEnd) - reinterpret_cast<char*>(pStart) - 1; }
+		size_t size() const { return reinterpret_cast<char*>(pCurrent) - reinterpret_cast<char*>(pStart); }
+
+	private:
+		void* pStart;
+		void* pCurrent;
+		void* pEnd; /* points past allocated memory */
+	};
+}
