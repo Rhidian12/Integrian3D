e4547c370009ce6e0b3a2234ee8a90f66ed805eb
Rhidian De Wit <rhidiandewit@gmail.com>
2023-01-21 10:34:08 +0100

    wip for memory allocation

--- Integrian3D/Memory/New/New.cpp
+++ Integrian3D/Memory/New/New.cpp
@@ -7,6 +7,9 @@ void* operator new(size_t n)
 {
 	using namespace Integrian3D;
 
+	if (n == 0) /* Standard dictates that 0 byte requests must return a valid pointer */
+		n = 1; /* So, make the size 1 byte instead of 0 */
+
 	Memory::MemoryTracker::Track(n);
 
 	void* pData{ malloc(n) };
@@ -19,6 +22,9 @@ void* operator new(size_t n)
 void* operator new[](size_t n)
 {
 	using namespace Integrian3D;
+
+	if (n == 0) /* Standard dictates that 0 byte requests must return a valid pointer */
+		n = 1; /* So, make the size 1 byte instead of 0 */
 
 	Memory::MemoryTracker::Track(n);
 
