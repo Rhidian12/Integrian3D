b0b289eed4415a279777f404f9208ad5be0ff80d
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2023-02-07 21:57:56 +0100

    added a few extra file tests

--- Integrian3D/main.cpp
+++ Integrian3D/main.cpp
@@ -1576,6 +1576,35 @@ TEST_CASE("Testing the File")
 		REQUIRE(file.GetLine() == "This is a test string\n");
 		REQUIRE(file.GetLine() == "This is a second test string on a new line");
 	}
+
+	SECTION("Writing to a file some text using a specific delimiter")
+	{
+		File file{ "Resources/TestASCIIFile.txt" };
+
+		file.ClearBuffer();
+		REQUIRE(file.GetBuffer().Size() == 0);
+
+		file << 5 << ',';
+		file << 16.f << ',';
+		file << "This is a test string,";
+		file << "This is a second test string on a new line";
+
+		REQUIRE(file.GetBuffer().Size() > 0);
+
+		file.Write();
+	}
+
+	SECTION("Reading a file with some text using a specific delimiter")
+	{
+		File file{ "Resources/TestASCIIFile.txt" };
+
+		REQUIRE(file.GetBuffer().Size() > 0);
+
+		REQUIRE(std::stoi(file.GetLine(',')) == 5);
+		REQUIRE(Math::AreEqual(std::stof(file.GetLine(',')), 16.f));
+		REQUIRE(file.GetLine(',') == "This is a test string,");
+		REQUIRE(file.GetLine(',') == "This is a second test string on a new line");
+	}
 }
 
 #endif FILE_TESTS
