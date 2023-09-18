f9969d8197df1fedad3ddfc7898556d7e708bc18
Rhidian De Wit <rhidiandewit@gmail.com>
2023-09-18 23:49:32 +0200

    remoev hardcoded cube

--- Integrian3D/main.cpp
+++ Integrian3D/main.cpp
@@ -47,91 +47,29 @@ int main(int argc, char* argv[])
 #include "Components/TransformComponent/TransformComponent.h"
 #include "Timer/Timer.h"
 #include "Memory/Allocator/Allocator.h"
+#include "Memory/UniquePtr.h"
 #include "Components/TestRotateComponent/TestRotateComponent.h"
 #include "Components/FreeCameraComponent/FreeCameraComponent.h"
 
 int RunTestEngine(int, char* [])
 {
 	using namespace Integrian3D;
-	using namespace Integrian3D::Memory;
 
 	Core& core{ Core::CreateCore(1080,720) };
 
-	//TArray<Vertex> vertices =
-	//{
-	//	Vertex{ glm::vec3{ -0.5f, -0.5f, -0.5f },  glm::vec2{ 0.0f, 0.0f } },
-	//	Vertex{ glm::vec3{  0.5f, -0.5f, -0.5f },  glm::vec2{ 1.0f, 0.0f } },
-	//	Vertex{ glm::vec3{  0.5f,  0.5f, -0.5f },  glm::vec2{ 1.0f, 1.0f } },
-	//	Vertex{ glm::vec3{  0.5f,  0.5f, -0.5f },  glm::vec2{ 1.0f, 1.0f } },
-	//	Vertex{ glm::vec3{ -0.5f,  0.5f, -0.5f },  glm::vec2{ 0.0f, 1.0f } },
-	//	Vertex{ glm::vec3{ -0.5f, -0.5f, -0.5f },  glm::vec2{ 0.0f, 0.0f } },
-
-	//	Vertex{ glm::vec3{ -0.5f, -0.5f,  0.5f },  glm::vec2{ 0.0f, 0.0f } },
-	//	Vertex{ glm::vec3{  0.5f, -0.5f,  0.5f },  glm::vec2{ 1.0f, 0.0f } },
-	//	Vertex{ glm::vec3{  0.5f,  0.5f,  0.5f },  glm::vec2{ 1.0f, 1.0f } },
-	//	Vertex{ glm::vec3{  0.5f,  0.5f,  0.5f },  glm::vec2{ 1.0f, 1.0f } },
-	//	Vertex{ glm::vec3{ -0.5f,  0.5f,  0.5f },  glm::vec2{ 0.0f, 1.0f } },
-	//	Vertex{ glm::vec3{ -0.5f, -0.5f,  0.5f },  glm::vec2{ 0.0f, 0.0f } },
-
-	//	Vertex{ glm::vec3{ -0.5f,  0.5f,  0.5f },  glm::vec2{ 1.0f, 0.0f } },
-	//	Vertex{ glm::vec3{ -0.5f,  0.5f, -0.5f },  glm::vec2{ 1.0f, 1.0f } },
-	//	Vertex{ glm::vec3{ -0.5f, -0.5f, -0.5f },  glm::vec2{ 0.0f, 1.0f } },
-	//	Vertex{ glm::vec3{ -0.5f, -0.5f, -0.5f },  glm::vec2{ 0.0f, 1.0f } },
-	//	Vertex{ glm::vec3{ -0.5f, -0.5f,  0.5f },  glm::vec2{ 0.0f, 0.0f } },
-	//	Vertex{ glm::vec3{ -0.5f,  0.5f,  0.5f },  glm::vec2{ 1.0f, 0.0f } },
-
-	//	Vertex{ glm::vec3{  0.5f,  0.5f,  0.5f },  glm::vec2{ 1.0f, 0.0f } },
-	//	Vertex{ glm::vec3{  0.5f,  0.5f, -0.5f },  glm::vec2{ 1.0f, 1.0f } },
-	//	Vertex{ glm::vec3{  0.5f, -0.5f, -0.5f },  glm::vec2{ 0.0f, 1.0f } },
-	//	Vertex{ glm::vec3{  0.5f, -0.5f, -0.5f },  glm::vec2{ 0.0f, 1.0f } },
-	//	Vertex{ glm::vec3{  0.5f, -0.5f,  0.5f },  glm::vec2{ 0.0f, 0.0f } },
-	//	Vertex{ glm::vec3{  0.5f,  0.5f,  0.5f },  glm::vec2{ 1.0f, 0.0f } },
-
-	//	Vertex{ glm::vec3{ -0.5f, -0.5f, -0.5f },  glm::vec2{ 0.0f, 1.0f } },
-	//	Vertex{ glm::vec3{  0.5f, -0.5f, -0.5f },  glm::vec2{ 1.0f, 1.0f } },
-	//	Vertex{ glm::vec3{  0.5f, -0.5f,  0.5f },  glm::vec2{ 1.0f, 0.0f } },
-	//	Vertex{ glm::vec3{  0.5f, -0.5f,  0.5f },  glm::vec2{ 1.0f, 0.0f } },
-	//	Vertex{ glm::vec3{ -0.5f, -0.5f,  0.5f },  glm::vec2{ 0.0f, 0.0f } },
-	//	Vertex{ glm::vec3{ -0.5f, -0.5f, -0.5f },  glm::vec2{ 0.0f, 1.0f } },
-
-	//	Vertex{ glm::vec3{ -0.5f,  0.5f, -0.5f },  glm::vec2{ 0.0f, 1.0f } },
-	//	Vertex{ glm::vec3{  0.5f,  0.5f, -0.5f },  glm::vec2{ 1.0f, 1.0f } },
-	//	Vertex{ glm::vec3{  0.5f,  0.5f,  0.5f },  glm::vec2{ 1.0f, 0.0f } },
-	//	Vertex{ glm::vec3{  0.5f,  0.5f,  0.5f },  glm::vec2{ 1.0f, 0.0f } },
-	//	Vertex{ glm::vec3{ -0.5f,  0.5f,  0.5f },  glm::vec2{ 0.0f, 0.0f } },
-	//	Vertex{ glm::vec3{ -0.5f,  0.5f, -0.5f },  glm::vec2{ 0.0f, 1.0f } }
-	//};
-
-	//TArray<uint32_t> indices{};
-
-	//for (uint32_t i{}; i < vertices.Size(); ++i)
-	//{
-	//	indices.Add(i);
-	//}
-
 	TextureManager::GetInstance().AddTexture("__Wall", "Resources/wall.jpg");
 
-	Scene* pTestScene{ new Scene{ "TestScene" } };
-	SceneManager::GetInstance().AddScene(pTestScene);
+	UniquePtr<Scene> TestScene{ new Scene{ "TestScene" } };
+	SceneManager::GetInstance().AddScene(TestScene.Get());
 
 	{
-		Entity entity{ pTestScene->CreateEntity() };
-		// pTestScene->AddComponent<TestRotateComponent>(entity);
-		pTestScene->AddComponent<MeshComponent>(entity, "Resources/StanfordBunny2.obj", TextureManager::GetInstance().GetTexture("__Wall"));
-		pTestScene->GetComponent<TransformComponent>(entity).Rotate(Math::Vec3D{ 0.f, Math::ToRadians(180.f), 0.f });
-		// pTestScene->GetComponent<TransformComponent>(entity).Scale(Math::Vec3D{ 0.01, 0.01, 0.01 });
+		Entity entity{ TestScene->CreateEntity() };
+		TestScene->AddComponent<TestRotateComponent>(entity);
+		TestScene->AddComponent<MeshComponent>(entity, "Resources/Spider.obj", TextureManager::GetInstance().GetTexture("__Wall"));
+		TestScene->GetComponent<TransformComponent>(entity).Rotate(Math::Vec3D{ 0.f, Math::ToRadians(180.f), 0.f });
 	}
 
-	//for (size_t i{}; i < 9; ++i)
-	//{
-	//	Entity entity{ pTestScene->CreateEntity() };
-	//	pTestScene->AddComponent<TestRotateComponent>(entity);
-	//	pTestScene->AddComponent<MeshComponent>(entity, vertices, indices, TextureManager::GetInstance().GetTexture("__Wall"));
-	//	pTestScene->GetComponent<TransformComponent>(entity).Rotate(Math::Vec3D{ Math::ToRadians(-55.f), 0.f, 0.f });
-	//	pTestScene->GetComponent<TransformComponent>(entity).Translate(Math::RandomVec3D(-5.f, 5.f));
-	//}
-
-	pTestScene->AddUpdateCallback(0, [](Scene& scene)->void
+	TestScene->AddUpdateCallback(0, [](Scene& scene)->void
 		{
 			if (scene.CanViewBeCreated<TestRotateComponent, TransformComponent>())
 			{
@@ -143,8 +81,6 @@ int RunTestEngine(int, char* [])
 		});
 
 	core.Run();
-
-	delete pTestScene;
 
 	return 0;
 }
