705ff6e146c8611300b858807d87dcd0f30c67c3
Rhidian De Wit <rhidiandewit@gmail.com>
2023-11-25 15:47:45 +0100

    start editor work

--- I3Editor/main.cpp
+++ I3Editor/main.cpp
@@ -1,5 +1,20 @@
+#include "Core/Core.h"
+
 #include <imgui.h>
+
+#if _WIN32
+#	include <Windows.h>
+#endif
 
 int main(int argc, char* argv[])
 {
+	using namespace Integrian3D;
+
+	// size of screen in pixels excluding size of taskbar
+	int Width = GetSystemMetrics(SM_CXFULLSCREEN);
+	int Height = GetSystemMetrics(SM_CYFULLSCREEN);
+
+	Core& EditorCore{ Core::CreateCore(Width, Height) };
+
+	EditorCore.Run();
 }
